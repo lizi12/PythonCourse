48a3c508b4cfc67c2ca5f53fb3f8d39f3a334459
lizi12 <48433549+lizi12@users.noreply.github.com>
2019-03-11 12:15:14 +0200

    Update hw1_question3.py

--- hw1_question3.py
+++ hw1_question3.py
@@ -17,8 +17,8 @@ def compare_subjects_within_student(subj1_all_students,
             res[key] = subj2_all_students["course_name"]
     return res
 
-table = compare_subjects_within_student(math_all_students, history_all_students)
-print(table)                                  
+#table = compare_subjects_within_student(math_all_students, history_all_students)
+#print(table)                                  
 
 if __name__ == '__main__':
     # Question 3
@@ -27,23 +27,23 @@ if __name__ == '__main__':
     the students names are the keys and their grades are the values (first exam is first).
     each dictionry inclueds subject key which its value is the subject name at the end of the dict. """
     math_all_students = {
-        "course_name": "math",
-        "Kelly": [78,100],
-        "Brenda": [100,90],
-        "Noam": [87,59],
-        "Yael": [95,89],
-        "Yotam": [58,76],
-        "Gili": [88,85]
+    "course_name": "math",
+    "Kelly": [78,100],
+    "Brenda": [100,90],
+    "Noam": [87,59],
+    "Yael": [95,89],
+    "Yotam": [58,76],
+    "Gili": [88,85]
     }
 
     history_all_students = {
-        "course_name": "history",
-        "Brenda": [94,99],
-        "Noam": [60,100],
-        "Yael": [85,62],
-        "Yotam": [55,91],
-        "Gili": [70,98],
-        "Dino": [50,50]
+    "course_name": "history",
+    "Brenda": [94,99],
+    "Noam": [60,100],
+    "Yael": [85,62],
+    "Yotam": [55,91],
+    "Gili": [70,98],
+    "Dino": [50,50]
     }
     subj1_all_students = math_all_students
     subj2_all_students = history_all_students
